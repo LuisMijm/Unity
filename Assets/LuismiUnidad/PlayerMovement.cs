45aee4b68feb30f565447e26a39c53f5f92649a3
LuisMijm <99535966+LuisMijm@users.noreply.github.com>
2022-02-18 12:47:15 +0100

    Add files via upload

---
+++ Assets/LuismiUnidad/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Transform tr_;
+    private Rigidbody rb_;
+    private Vector3 inputDir_;
+    public float speed_;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tr_ = GetComponent<Transform>();
+        rb_ = GetComponent<Rigidbody>();
+        inputDir_ = new Vector3();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputDir_ = tr_.forward * Input.GetAxis("Vertical");
+        inputDir_ += tr_.right * Input.GetAxis("Horizontal");
+
+        Vector3.Normalize(inputDir_);
+
+
+        tr_.position += inputDir_ * speed_ * Time.deltaTime;
+        //tr_.Translate(inputDir_ * speed_ * Time.deltaTime, Space.World);
+
+
+
+    }
+
+    void FicedUpdate()
+    {
+
+
+        //rb_.velocity =
+        //rb_.AddForce();
+    }
+}
---
+++ Assets/LuismiUnidad/Player_Physics_Movement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Physics_Movement : MonoBehaviour
+{
+
+  private Transform tr_;
+  private Rigidbody rb_;
+  private Vector3 inputDir_;
+  public float forceApplied_;
+  public float impulse_;
+  public float velocityChange_;
+  private Vector3 targetVelocity_;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+          tr_ = GetComponent<Transform>();
+          rb_ = GetComponent<Rigidbody>();
+          inputDir_ = new Vector3();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+      inputDir_.z = Input.GetAxis("Vertical");
+      inputDir_.x = Input.GetAxis("Horizontal");
+
+      inputDir_ = Vector3.Normalize(inputDir_);
+
+      if(Input.GetButtonDown("Fire1"))
+      {
+        rb_.AddForce(Vector3.up * impulseApplied_,ForceMode.Impulse);
+      }
+    }
+
+    void FixedUpdate()  //Called every 20 ms by default
+    {
+      // rb_.AddForce(inputDir_ * forceApplied_, ForceMode.Force); //*Time.fixedDeltaTime
+      // rb_.AddForce(inputDir_ * forceApplied_, ForceMode.Acceleration);
+      // rb_.AddForce(inputDir_ * velocityChange_ * Time.fixedDeltaTime, ForceMode.velocityChange_)
+      rb_.velocity = inputDir_ * velocityChange_;
+    }
+}
